634321796f9035afe3f2f0312af7004487780a1e
jiangbangfa <976951401@qq.com>
2022-06-20 13:10:54 +0800

    更新：客户单位切换页面

--- finance-frontend/src/pages/SwitchCustomer.jsx
+++ finance-frontend/src/pages/SwitchCustomer.jsx
@@ -1,16 +1,15 @@
-import {Button, Col, Input, message, Modal, Row, Space} from "antd";
+import {Button, Card, Col, Input, message, Row, Space} from "antd";
 import ExProTable from "@/components/Table/ExtProTable";
 import React, {useRef, useState} from "react";
 import {ownedCustomerUsingGET, switchProxyCustomerUsingPUT} from "@/services/swagger/userWeb";
-import {history, useModel} from "umi"
-import {loginOut} from "@/services/login";
+import {useModel} from "umi"
+import GlobalPageContainer from "@/components/PageContainer";
 
 
 export default function SwitchCustomer() {
   const actionRef = useRef()
   const [customerName, setCustomerName] = useState()
   const [customerNumber, setCustomerNumber] = useState()
-  const { setInitialState } = useModel('@@initialState')
   const [selectedRowKeys, setSelectedRowKeys] = useState([])
   const columns = [
     {
@@ -31,52 +30,45 @@ export default function SwitchCustomer() {
     })
   }
   return (
-    <Modal title="选择客户单位" width={600} visible={true} closable={false}
-           footer={[
-             <Button key="quit" onClick={() => {
-               setInitialState((v) => ({ ...v, currentUser: undefined }))
-               loginOut()
-             }}>
-               退出登录
-             </Button>,
-             <Button key="submit" type="primary" onClick={handleSwitchCustomer}>
-               确认
-             </Button>
-           ]}
-    >
-      <Space>
-        <Row align="middle">
-          <Col span={5}>编号：</Col>
-          <Col span={19}>
-            <Input allowClear onChange={(e) => setCustomerNumber(e.target.value || undefined)}/>
-          </Col>
-        </Row>
-        <Row align="middle">
-          <Col span={5}>名称：</Col>
-          <Col span={19}>
-            <Input allowClear onChange={(e) => setCustomerName(e.target.value || undefined)}/>
-          </Col>
-        </Row>
-        <Button type="primary" onClick={actionRef.current?.reload}>查询</Button>
-      </Space>
-      <ExProTable actionRef={actionRef} columns={columns}
-                  scroll={{y: 600}} editable={false}
-                  toolBarRender={false} search={false}
-                  tableAlertRender={false}
-                  onRow={(record) => ({
-                    onClick: () => setSelectedRowKeys([record.id])
-                  })}
-                  rowSelection={{
-                    type: 'radio',
-                    selectedRowKeys: selectedRowKeys,
-                    onChange: (keys) => {
-                      setSelectedRowKeys(keys)
-                    }
-                  }}
-                  request={() => {
-                    return ownedCustomerUsingGET({customerNumber, customerName})
-                  }}
-      />
-    </Modal>
+    <GlobalPageContainer>
+      <Card title="选择接户单位">
+        <Space>
+          <Row align="middle">
+            <Col span={5}>编号：</Col>
+            <Col span={19}>
+              <Input allowClear onChange={(e) => setCustomerNumber(e.target.value || undefined)}/>
+            </Col>
+          </Row>
+          <Row align="middle">
+            <Col span={5}>名称：</Col>
+            <Col span={19}>
+              <Input allowClear onChange={(e) => setCustomerName(e.target.value || undefined)}/>
+            </Col>
+          </Row>
+          <Button type="primary" onClick={actionRef.current?.reload}>查询</Button>
+        </Space>
+        <ExProTable actionRef={actionRef} columns={columns}
+                    scroll={{y: 600}} editable={false}
+                    toolBarRender={false} search={false}
+                    tableAlertRender={false}
+                    onRow={(record) => ({
+                      onClick: () => setSelectedRowKeys([record.id])
+                    })}
+                    rowSelection={{
+                      type: 'radio',
+                      selectedRowKeys: selectedRowKeys,
+                      onChange: (keys) => {
+                        setSelectedRowKeys(keys)
+                      }
+                    }}
+                    request={() => {
+                      return ownedCustomerUsingGET({customerNumber, customerName})
+                    }}
+        />
+        <Button key="submit" type="primary" onClick={handleSwitchCustomer}>
+          确认
+        </Button>
+      </Card>
+    </GlobalPageContainer>
   )
 }
