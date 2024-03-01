030e1afb0f343527daaa4f8ccde73f070e64d103
jiangbangfa <976951401@qq.com>
2024-03-01 10:47:00 +0800

    test

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM openjdk:8
-
-COPY build/libs/finance-1.0-SNAPSHOT.jar app.jar
--- src/main/java/org/finance/infrastructure/constants/MapStructExp.java
+++
@@ -1,9 +0,0 @@
-package org.finance.infrastructure.constants;
-
-/**
- * @author jiangbangfa
- */
-public class MapStructExp {
-
-    public final static String CUSTOMER_ID = "java(org.finance.infrastructure.config.security.util.SecurityUtil.getCustomerId())";
-}
